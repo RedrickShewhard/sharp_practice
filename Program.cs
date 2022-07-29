badf09ccac6fef8a12d34d01371d6c2eaec1be89
Peter <p.shishonkov@yandex.ru>
2022-07-29 23:23:55 +0300

    add program solution

--- Program.cs
+++ Program.cs
@@ -3,9 +3,27 @@
 Первоначальный массив можно ввести с клавиатуры либо задать 
 на старте выполнения алгоритма. */
 
-//1. создание массива
-//2. заполнение массива
-//3. главная задача
-//4. печать массива
+string alpha = "symbols_and_words";
+string[] array = new string[30];
+string[] arrayres = new string[30];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    int wordLength = new Random().Next(1, 10);
+    string word = alpha.Substring(0, wordLength);
+    array[i] = word;
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        arrayres[j] = array[i];
+        j++;
+    }
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(arrayres[i] + " ");
+}
 
 
